c90fe1c393dc4a44adf1a54cb8a6afc009173c54
Oliver Martinez <oliveratom772@gmail.com>
2024-02-18 20:13:18 -0600

    Eliminacion de una linea que no hacia nada

--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -3,7 +3,6 @@ import Layout from "../../Components/Layout"
 import OrdersCard from "../../Components/OrdersCard"
 import { ShoppingCartContext } from "../../Context"
 import { Link } from "react-router-dom"
-import { ChevronLeftIcon } from "@heroicons/react/24/solid"
 
 function MyOrders() {
   const context = useContext(ShoppingCartContext)
